c0a75e14e37342a62ba75e08e3e4b07e8051d999
Marina Kosolap <70973716+Liveently@users.noreply.github.com>
2022-09-06 16:18:22 +0700

    Add files via upload

---
+++ untitled2/getName.cpp
@@ -0,0 +1,11 @@
+#include "getName.h"
+
+namespace getName {
+
+    std::string getMyName()
+    {
+        std::string name = "Peter";
+        return name;
+
+    }
+}
---
+++ untitled2/getName.h
@@ -0,0 +1,7 @@
+#include <string>
+
+namespace getName {
+
+    std::string getMyName();
+
+}
